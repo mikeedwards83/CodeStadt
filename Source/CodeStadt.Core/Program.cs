b3c4f0d9bf11643cd7e83a1d0c324db3296c43a0
Michael Edwards <mikeedwards83@googlemail.com>
2011-02-12 20:22:01 +0000

    initial project setup

---
+++ Source/CodeStadt.Core/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using Driven.Metrics.Reporting;
+using Driven.Metrics.metrics;
+
+namespace CodeStadt.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var something =  new Driven.Metrics.DrivenMetrics.Factory().Create(
+                new string[] { "CodeStadt.Core.exe" },
+                new IMetricCalculator[]{
+                    new NumberOfLinesCalculator(300),
+                    new ILCyclomicComplextityCalculator(4)},
+                    "TestReport",
+                    (new ReportFactory()).ResolveReport(ReportType.All, "TestReport"));
+           
+            something.RunAllMetricsAndGenerateReport();
+                    
+
+
+
+
+        }
+    }
+}
